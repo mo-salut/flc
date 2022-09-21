000d423c3a9e067ce0c77ba4c975c92e26b7018d
mosalut <mosalut@gmail.com>
2022-09-21 15:17:49 +0800

    Change the wallet addresses Add burn function Config for the goerli testnet Move test code from script/deploy.js to test/Flc.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,19 @@ module.exports = {
 	networks: {
 		local: {
 			url: "http://127.0.0.1:8545"
+		},
+		goerli: {
+			url: "https://eth-goerli.g.alchemy.com/v2/HF9zrMo6C1SqvhXgTIZ5Ytw5KPYMsWw6",
+			accounts: ["a011bd23f32d3069d285d5d0119e5d5ff1ea62c9863f79b0d3f3585ab0b96f3a"]
 		}
 	},
-	solidity: "0.8.9"
+	solidity: {
+		version: "0.8.9",
+		settings: {
+			optimizer: {
+			enabled: true,
+			runs: 200
+			}
+		}
+	}
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,25 +1,12 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
 const hre = require("hardhat");
 
 async function main() {
-//	const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-//	const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-//	const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
-
-//	const lockedAmount = hre.ethers.utils.parseEther("1");
-
 	const Flc = await hre.ethers.getContractFactory("Flc");
-//	const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
 	const flc = await Flc.deploy();
 
 	await flc.deployed();
 
-	console.log(`FLC deployed to ${flc.address}`);
+	/*
 	let owner = ethers.provider.getSigner(0);
 	let inverser = ethers.provider.getSigner(1);
 	let team = ethers.provider.getSigner(2);
@@ -35,31 +22,10 @@ async function main() {
 	console.log("fund:", hre.ethers.utils.formatEther(await flc.balanceOf(fund.getAddress()) + ""));
 	console.log("uni:", hre.ethers.utils.formatEther(await flc.balanceOf(uni.getAddress()) + ""));
 	console.log();
-
-	console.log("locked:", hre.ethers.utils.formatEther(await flc.lockOf(inverser.getAddress())));
-	console.log("locked:", hre.ethers.utils.formatEther(await flc.lockOf(team.getAddress())));
-	console.log("locked:", hre.ethers.utils.formatEther(await flc.lockOf(repository.getAddress())));
-	console.log();
-
-	console.log((await flc.unlocktimeOf(inverser.getAddress())).toNumber());
-	console.log((await flc.unlocktimeOf(team.getAddress())).toNumber());
-	console.log((await flc.unlocktimeOf(repository.getAddress())).toNumber());
-	console.log((await flc.unlocktimeOf(owner.getAddress())).toNumber());
-	console.log();
-
-	console.log(await flc.connect(uni).transfer(inverser.getAddress(), hre.ethers.utils.parseEther("10000")));
-//	console.log(await flc.connect(inverser).transfer(owner.getAddress(), hre.ethers.utils.parseEther("20000")));
-	console.log(await flc.connect(inverser).approve(owner.getAddress(), hre.ethers.utils.parseEther("20000")));
-	console.log(await flc.transferFrom(inverser.getAddress(), owner.getAddress(), hre.ethers.utils.parseEther("20000")));
-	/*
-	console.log(
-	`Lock with 1 ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-	);
 	*/
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
 	console.error(error);
 	process.exitCode = 1;
